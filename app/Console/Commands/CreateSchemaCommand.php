7a194da927051bd5639fc85d055612e3d5be3854
Diogo Silva <silva.diogo1990@gmail.com>
2017-09-15 21:18:24 +0100

    Created command to create database

--- app/Console/Commands/CreateSchemaCommand.php
+++ app/Console/Commands/CreateSchemaCommand.php
@@ -1,7 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: diogosilva
- * Date: 11/09/2017
- * Time: 01:24
- */
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateSchemaCommand extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'create-schema';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Creates the database schema";
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $schemaName = $this->input->getOption('schemaName');
+
+	    return DB::statement(DB::raw("CREATE DATABASE IF NOT EXISTS $schemaName"));
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            array('schemaName', null, InputOption::VALUE_OPTIONAL, 'The schema name to be created', env('DB_NAME')),
+        );
+    }
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		return $this->laravel->call([$this, 'fire']);
+	}
+
+}
