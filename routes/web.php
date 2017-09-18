5e0291a9c1f4aa20793e0fe38c7737a59532bac2
Diogo Silva <silva.diogo1990@gmail.com>
2017-09-18 03:41:42 +0100

    defining the api routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,25 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->get( '/', function () use ( $router ) {
+	return $router->app->version();
+} );
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->group( [ 'prefix' => 'discounts/v1' ], function ( $router ) {
+	//Customer routes
+	$router->get( 'customer', 'CustomerController@index' );
+	$router->post( 'customer', 'CustomerController@createCustomer' );
+	$router->put( 'customer', 'CustomerController@updateCustomer' );
+	$router->delete( 'customer', 'CustomerController@deleteCustomer' );
+
+	//product routes
+	$router->get( 'product', 'ProductController@index' );
+	$router->post( 'product', 'ProductController@createProduct' );
+	$router->put( 'product', 'ProductController@updateProduct' );
+	$router->delete( 'product', 'ProductController@deleteProduct' );
+
+	//order routes
+	$router->get( 'order', 'OrderController@index' );
+	$router->post( 'order', 'OrderController@createOrder' );
+} );
+
