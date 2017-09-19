5d171d82fc6152fb94dca2bddd6839d0434f40a6
Diogo Silva <silva.diogo1990@gmail.com>
2017-09-19 01:33:34 +0100

    First discounts business logic sketch. Still needs alot of work and refactoring :hammer:

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,50 +14,172 @@ use App\Product;
 use App\Customer;
 use Illuminate\Http\Request;
 use Mockery\Exception;
+use phpDocumentor\Reflection\Types\Integer;
 
 class OrderController extends Controller {
 
 	public function index() {
 		$orders = Order::all();
 
-		return response()->json($orders);
+		return response()->json( $orders );
 	}
 
-	public function createOrder(Request $request) {
+	public function createOrder( Request $request ) {
 
 		try {
-			$order = new Order();
-			$order->id = $request->input('id');
+			$order     = new Order();
+			$order->id = $request->input( 'id' );
 
 			// Get customer and associate it to the order
-			$customer = Customer::find($request->input('customer-id'));
+			$customer = Customer::find( $request->input( 'customer-id' ) );
 			$order->customer()->associate( $customer );
 
-			$order->total = $request->input('total');
+			$total = $request->input( 'total' );
+
+			//@TODO refact this to happen on associate belongsTo relationship
+			$customer->revenue += $total;
+
+			$order->total = $total;
 			$order->saveOrFail();
 
-			$items = $request->input('items');
-			foreach ($items as $item) {
+			$items = $request->input( 'items' );
+			foreach ( $items as $item ) {
 				$newItem = new Item();
 
 				//Associate the item to the order
 				$newItem->order()->associate( $order );
 
 				//Find the product and associate it to the item
-				$product = Product::find($item['product-id']);
-				$newItem->product()->associate($product);
+				$product = Product::find( $item['product-id'] );
+				$newItem->product()->associate( $product );
 
 				$newItem->quantity = $item['quantity'];
 
 				//Inserting item directly in the relation with the order
-				$order->items()->saveOrFail($newItem);
+				$order->items()->save( $newItem );
 			}
 
-			return response()->json($order);
+			$discounts = $this->calculateDiscount( $order );
 
-		} catch (Exception $exception) {
-			return response()->json('Failed with message: ' . $exception->getMessage());
+			return response()->json( [$order, $discounts] );
+
+		} catch ( Exception $exception ) {
+			return response()->json( 'Failed with message: ' . $exception->getMessage() );
 		}
 
+	}
+
+	private function calculateDiscount( Order $order ) {
+		return [
+			$this->discountRule1($order),
+			$this->discountRule2($order),
+			$this->discountRule3($order)
+		];
+	}
+
+	private function discountRule1( $order ) {
+		$discount = [
+			'total' => 0,
+			'description' => 'A customer who has already bought for over € 1000, gets a discount of 10% on the whole order.'
+		];
+
+		$customer = $order->customer;
+
+		if ( $customer->revenue >= 1000 ) {
+			$discount['total'] = $order->total * 0.10;
+		}
+
+		return $discount;
+	}
+
+	private function discountRule3( $order ) {
+		$unitPrice = 0;
+		$totalSwitches = $this->countByCategory( $order, Product::CATEGORY_SWITCHES );
+
+		$freeSwitches = $this->freeSwitches( $totalSwitches );
+
+		$items = $order->items;
+
+		foreach ( $items as $item ) {
+			$product = $item->product;
+			if ( $product->category == Product::CATEGORY_SWITCHES ) {
+				$unitPrice = $product->price;
+			}
+		}
+
+		return [
+			'total' => $freeSwitches * $unitPrice,
+			'switches' => $totalSwitches,
+			'description' => 'For every products of category "Switches" (id 2), when you buy five, you get a sixth for free.'
+		];
+	}
+
+	/**
+	 * Returns the quantity of free switches in the order
+	 *
+	 * @param $total
+	 * @param int $freeSwitches
+	 *
+	 * @return int
+	 */
+	private function freeSwitches( $total, $freeSwitches = 0 ) {
+
+		if ( ( $total - 6 ) >= 0 ) {
+			$freeSwitches ++;
+
+			return $this->freeSwitches( $total - 6, $freeSwitches );
+		}
+
+		return $freeSwitches;
+	}
+
+
+	private function discountRule2( Order $order ) {
+		$discount = [
+			'total' => 0,
+			'tools' => 0,
+			'description' => 'If you buy two or more products of category "Tools" (id 1), you get a 20% discount on the cheapest product.'
+		];
+
+		$totalTools = $this->countByCategory( $order, Product::CATEGORY_TOOLS );
+		if ( $totalTools > 1 ) {
+			$discount['total'] = $this->getCheapestProduct( $order )->price;
+		}
+
+		$discount['tools'] = $totalTools;
+
+		return $discount;
+
+	}
+
+	/**
+	 * Counts the Products with a given category present in a Order
+	 *
+	 * @param Order $order
+	 * @param $category
+	 *
+	 * @return int
+	 */
+	private function countByCategory( Order $order, $category ) {
+		$items = $order->items();
+
+		return (int) $items->join( 'products', 'items.product_id', '=', 'products.id' )
+		             ->where( 'products.category', '=', $category )
+		             ->sum('items.quantity');
+	}
+
+	/**
+	 * Return cheapest Product in the Order
+	 *
+	 * @param Order $order
+	 *
+	 * @return mixed
+	 */
+	private function getCheapestProduct( Order $order ) {
+
+		return $order->items()
+		             ->join( 'products', 'items.product_id', '=', 'products.id' )
+		             ->orderBy( 'products.price', 'asc' )
+		             ->first();
 	}
 }
